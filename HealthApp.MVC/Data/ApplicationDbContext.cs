612a6539c7c47baa132ee17b4b5a7b478343d20f
théo <irujotheo@gmail.com>
2025-03-12 15:14:37 +0000

    Add Sqlite Database

--- HealthApp.MVC/Data/ApplicationDbContext.cs
+++ HealthApp.MVC/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using HealthApp.MVC.Models;
 
 namespace HealthApp.MVC.Data;
 
@@ -8,5 +9,18 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=database.db");
+        }
     }
 }
